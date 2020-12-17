c5e1c5e7c6915fde0d6ef68b68fc6e912cf10621
Илья <lionson@mail.ru>
2020-12-17 20:51:23 +0200

    split long lines

--- wildberries/moscow_headers.py
+++ wildberries/moscow_headers.py
@@ -1,9 +1,11 @@
 headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;"
+              "q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Language": "ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
-    # "Cookie": "route=b4cf43292129b7aa0dbb9e0660e75f34a90eb854; __pricemargin=1.0--; __sppfix=; __catalogOptions=Sort%3APopular%26CardSize%3Ac246x328; __spp=0; _wbauid=816829481608132677; BasketUID=553891e6-cc59-4d2f-aa56-83979616bb85; ___wbu=7bd63252-c562-404e-9633-4a138ef7ddb8.1608132676; ___wbs=81f7c1cc-15f3-4abf-8287-b0c84d27bfdd.1608132676; _gcl_au=1.1.639649937.1608132678; stories_uid=834241608132677992; _gid=GA1.2.1145542331.1608132678; _pk_ses.1.034e=*; .AspNetCore.Antiforgery.stpccMUKFUM=CfDJ8B3VpQcvGldFqgqMzU7RPJVOD0z6nozrg7d6RpSvxK_Z31Q-jLs-TQ9SLk5pt_pbu1_G08nOa8pmvML-H8F9m9BlWp_XPTZZLBvuxcHwrSMrqddgw06L8axhVuyxXbJg1qg3piZtKMITsiWd4y01Zvk; _ga_TW9NLWX9V5=GS1.1.1608131121.1.1.1608135757.59; _ga=GA1.2.782002727.1608131121; _dc_gtm_UA-2093267-1=1; _pk_id.1.034e=5005127b5fee8629.1608132678.1.1608135758.1608132678.; __wbl=cityId%3D77%26regionId%3D77%26city%3D%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%26phone%3D84957755505%26latitude%3D55%2C7247%26longitude%3D37%2C7882; __store=116433_115577_119400_117501_507_3158_120602_6158_2737_117986_1699_1733_117673_119261_117413_119070_118106_119781; __region=68_69_63_75_33_40_48_70_64_1_4_30_71_22_38_31_66; __cpns=3_12_15_18; ncache=116433_115577_119400_117501_507_3158_120602_6158_2737_117986_1699_1733_117673_119261_117413_119070_118106_119781%3B68_69_63_75_33_40_48_70_64_1_4_30_71_22_38_31_66%3B1.0--%3B3_12_15_18%3B%3B0%3BSort%3APopular%26CardSize%3Ac246x328",
     "Referer": "Referer: https://www.wildberries.ru/catalog/obuv/zhenskaya/sabo-i-myuli/myuli",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.60"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.60"
 }
 
 cookies = {
@@ -20,14 +22,20 @@ cookies = {
     "stories_uid": "834241608132677992",
     "_gid": "GA1.2.1145542331.1608132678",
     "_pk_ses.1.034e": "*",
-    ".AspNetCore.Antiforgery.stpccMUKFUM": "CfDJ8B3VpQcvGldFqgqMzU7RPJVOD0z6nozrg7d6RpSvxK_Z31Q-jLs-TQ9SLk5pt_pbu1_G08nOa8pmvML-H8F9m9BlWp_XPTZZLBvuxcHwrSMrqddgw06L8axhVuyxXbJg1qg3piZtKMITsiWd4y01Zvk",
+    ".AspNetCore.Antiforgery.stpccMUKFUM":
+        "CfDJ8B3VpQcvGldFqgqMzU7RPJVOD0z6nozrg7d6RpSvxK_Z31Q-jLs-TQ9SLk5pt_pbu1_G08nOa8pmvML"
+        "-H8F9m9BlWp_XPTZZLBvuxcHwrSMrqddgw06L8axhVuyxXbJg1qg3piZtKMITsiWd4y01Zvk",
     "_ga_TW9NLWX9V5": "GS1.1.1608131121.1.1.1608135757.59",
     "_ga": "GA1.2.782002727.1608131121",
     "_dc_gtm_UA-2093267-1": "1",
     "_pk_id.1.034e": "5005127b5fee8629.1608132678.1.1608135758.1608132678.",
-    "__wbl": "cityId%3D77%26regionId%3D77%26city%3D%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%26phone%3D84957755505%26latitude%3D55%2C7247%26longitude%3D37%2C7882",
-    "__store": "116433_115577_119400_117501_507_3158_120602_6158_2737_117986_1699_1733_117673_119261_117413_119070_118106_119781",
+    "__wbl": "cityId%3D77%26regionId%3D77%26city%3D%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%26"
+             "phone%3D84957755505%26latitude%3D55%2C7247%26longitude%3D37%2C7882",
+    "__store": "116433_115577_119400_117501_507_3158_120602_6158_2737_"
+               "117986_1699_1733_117673_119261_117413_119070_118106_119781",
     "__region": "68_69_63_75_33_40_48_70_64_1_4_30_71_22_38_31_66",
     "__cpns": "3_12_15_18",
-    " ncache": "116433_115577_119400_117501_507_3158_120602_6158_2737_117986_1699_1733_117673_119261_117413_119070_118106_119781%3B68_69_63_75_33_40_48_70_64_1_4_30_71_22_38_31_66%3B1.0--%3B3_12_15_18%3B%3B0%3BSort%3APopular%26CardSize%3Ac246x328"
+    " ncache": "116433_115577_119400_117501_507_3158_120602_6158_2737_117986_1699_1733_117673_"
+               "119261_117413_119070_118106_119781%3B68_69_63_75_33_40_48_70_64_1_4_30_71_22_38"
+               "_31_66%3B1.0--%3B3_12_15_18%3B%3B0%3BSort%3APopular%26CardSize%3Ac246x328"
 }
