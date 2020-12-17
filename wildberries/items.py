53065fde9a2a65a977d2c5091995481120d686f8
Илья <lionson@mail.ru>
2020-12-17 20:50:14 +0200

    remove unused import

--- wildberries/items.py
+++ wildberries/items.py
@@ -1,11 +1,5 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 from dataclasses import dataclass, field
 import time
-from typing import Any
 
 
 @dataclass
